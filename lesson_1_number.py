266ef696cc81b725fc52d60e6f9807d820dafa6a
andreyka101 <andrejkvasnin236@gmail.com>
2025-01-07 19:57:46 +0300

    first commit

---
+++ lesson_1_number.py
@@ -0,0 +1,73 @@
+
+
+# print(20)
+# rjvv
+# print(5 + 3)
+# print(5 - 3)
+# print(5 * 3)
+# print(30 / 3)
+
+
+
+# num = 4
+# print(num)
+# num = 9
+# print(num)
+# num = 1
+# print(num + 10)
+
+
+
+# red_car = 5
+# redСarSpeed = 5
+# w0orl6d2 = 0
+
+
+
+# num_2 = 8
+# print("hello world" , 1222 , num_2)
+
+
+
+# print("int =" , 5)
+# print("float =" , 5.2)
+# print("str =" , 'qww' , "2rr22")
+# print("bool =" , True , False)
+# print("null =" , None)
+
+
+
+# num_3 = None
+# num_3 = 54
+# print(num_3)
+
+
+
+# num = 5
+# print(num + 1)
+
+# num_2 = num + 2
+# print(num_2)
+
+# num = num + 3
+# print(num)
+
+
+
+# str_num = "wdwudbuwdu"
+# print(str_num)
+# str_num = 5
+# print(str_num)
+
+
+
+print("6 + 3 =" , 6 + 3)
+print("6 - 3 =" , 6 - 3)
+print("6 * 3 =" , 6 * 3)
+print("6 ** 3 =" , 6 ** 3)
+print("10 / 3 =" , 10 / 3)
+print("10 // 3 =" , 10 // 3)
+print("10 % 3 =" , 10 % 3)
+
+
+
